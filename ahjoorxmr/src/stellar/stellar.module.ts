21a14801b0e1df8c94213aabdc4ba905081224a5
mayami <queenoluwatosin15@gmail.com>
2026-04-24 05:03:42 +0100

    feat: implement Soroban ContractStateGuard with simulation and Redis caching

--- ahjoorxmr/src/stellar/stellar.module.ts
+++ ahjoorxmr/src/stellar/stellar.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { StellarService } from './stellar.service';
 import { StellarCircuitBreakerService } from './stellar-circuit-breaker.service';
+import { ContractStateGuard } from './contract-state-guard.service';
 import { WinstonLogger } from '../common/logger/winston.logger';
 
 @Module({
-  providers: [StellarService, StellarCircuitBreakerService, WinstonLogger],
-  exports: [StellarService, StellarCircuitBreakerService],
+  providers: [
+    StellarService,
+    StellarCircuitBreakerService,
+    ContractStateGuard,
+    WinstonLogger,
+  ],
+  exports: [StellarService, StellarCircuitBreakerService, ContractStateGuard],
 })
 export class StellarModule {}
